24ae559bec93d294f24c6e5e068523569368ad2a
Caio Jesus <caio.nascimento@dcomp.ufs.br>
2022-02-14 18:37:27 -0300

    feat: create dio http client

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:freeflow/app_widget.dart';
 import 'package:freeflow/core/inject/inject.dart';
 
-void main() {
+Future<void> main() async {
   Inject.init();
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
