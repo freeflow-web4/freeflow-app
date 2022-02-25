0977578e55608f6091029c842bac3c7594efd7c7
Raphael <raphaeldesouza@outlook.com>
2022-02-24 22:13:22 -0300

    feat: add swipeButton

--- lib/layers/presentation/pages/login/login_page.dart
+++ lib/layers/presentation/pages/login/login_page.dart
@@ -6,6 +6,7 @@ import 'package:freeflow/core/utils/text_themes_mixin.dart';
 import 'package:freeflow/layers/presentation/pages/login/controller/login_controller.dart';
 import 'package:freeflow/layers/presentation/widgets/adaptative_spacer_widget.dart';
 import 'package:freeflow/layers/presentation/widgets/secondary_button.dart';
+import 'package:freeflow/layers/presentation/widgets/swipe_button_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -21,47 +22,52 @@ class _LoginPageState extends State<LoginPage> with TextThemes {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: StandardColors.backgroundDark,
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: getProportionalHeightFromValue(context, 88),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const AdaptativeSpacer(
-                heightFactor: 0.2,
-              ),
-              Image.asset('assets/images/logo.png'),
-              const AdaptativeSpacer(
-                heightFactor: 0.2,
-              ),
-              textH5(
-                context,
-                text: 'Grab your\nFreeFlow Pouch',
-                color: Colors.white,
-                textAlign: TextAlign.center,
-              ),
-              const AdaptativeSpacer(
-                heightFactor: 0.2,
-              ),
-              SecondaryButton(
-                child: const Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Recover Account',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionalHeightFromValue(context, 88),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const AdaptativeSpacer(
+              heightValue: 129,
+            ),
+            Image.asset('assets/images/logo.png'),
+            const AdaptativeSpacer(
+              heightValue: 171,
+            ),
+            textH5(
+              context,
+              text: 'Grab your\nFreeFlow Pouch',
+              color: Colors.white,
+              textAlign: TextAlign.center,
+            ),
+            const AdaptativeSpacer(
+              heightValue: 8,
+            ),
+            SwipeButton(
+              onSwipe: () {},
+              text: "LET'S GO!",
+            ),
+            const AdaptativeSpacer(
+              heightValue: 171,
+            ),
+            SecondaryButton(
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Recover Account',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
-                onPressed: () {},
-                padding: const EdgeInsets.symmetric(
-                  vertical: smSpacing,
-                ),
               ),
-            ],
-          ),
+              onPressed: () {},
+              padding: const EdgeInsets.symmetric(
+                vertical: smSpacing,
+              ),
+            ),
+          ],
         ),
       ),
     );
