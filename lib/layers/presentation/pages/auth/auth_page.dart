c7f682a023088f715cb134003961bf8d48375200
Raphael <raphaeldesouza@outlook.com>
2022-03-09 20:40:19 -0300

    feat: adding auth page widgets

--- lib/layers/presentation/pages/auth/auth_page.dart
+++ lib/layers/presentation/pages/auth/auth_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:freeflow/core/translation/translation_service.dart';
 import 'package:freeflow/core/utils/colors_constants.dart';
-import 'package:freeflow/layers/presentation/pages/auth/widgets/keyboard_widget.dart';
+import 'package:freeflow/core/utils/spacing_constants.dart';
+import 'package:freeflow/layers/presentation/pages/auth/auth_controller.dart';
+import 'package:freeflow/layers/presentation/widgets/gradient_text_field_widget.dart';
+import 'package:freeflow/layers/presentation/widgets/translation_text_widget.dart';
+import 'package:get_it/get_it.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -10,6 +16,17 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
+  final authController = GetIt.I.get<AuthController>();
+  //TODO: remove if possible and make not necessary for GradientTextFieldWidget
+  final FocusNode pinInputNode = FocusNode();
+  final TextEditingController pinTextController = TextEditingController();
+
+  @override
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +34,34 @@ class _AuthPageState extends State<AuthPage> {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          KeyBoard(),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: mdSpacingx2),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TranslationText(textKey: 'login.authTitle'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: mdSpacingx2,
+            ),
+            child: Observer(
+              builder: (context) {
+                return GradientTextFieldWidget(
+                  onChanged: (_) {},
+                  isFieldValid:
+                      authController.pinFieldState != PinFieldState.invalid,
+                  textController: pinTextController,
+                  inputNode: pinInputNode,
+                  errorText: TranslationService.translate(
+                    context,
+                    'login.pinTextInputError',
+                  ),
+                  hintText: '',
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
