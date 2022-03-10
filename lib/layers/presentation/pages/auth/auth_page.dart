a16d6e05e2bc3b7aa75f2f5d2ef3a1e21cc004b7
Raphael <raphaeldesouza@outlook.com>
2022-03-10 09:53:14 -0300

    feat: add keyboard and arrow button to authPage

--- lib/layers/presentation/pages/auth/auth_page.dart
+++ lib/layers/presentation/pages/auth/auth_page.dart
@@ -3,9 +3,11 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:freeflow/core/translation/translation_service.dart';
 import 'package:freeflow/core/utils/colors_constants.dart';
 import 'package:freeflow/core/utils/spacing_constants.dart';
+import 'package:freeflow/core/utils/text_themes_mixin.dart';
 import 'package:freeflow/layers/presentation/pages/auth/auth_controller.dart';
+import 'package:freeflow/layers/presentation/widgets/animated_arrow_right_widget.dart';
 import 'package:freeflow/layers/presentation/widgets/gradient_text_field_widget.dart';
-import 'package:freeflow/layers/presentation/widgets/translation_text_widget.dart';
+import 'package:freeflow/layers/presentation/widgets/in_app_keyboard/in_app_keyboard_widget.dart';
 import 'package:get_it/get_it.dart';
 
 class AuthPage extends StatefulWidget {
@@ -15,7 +17,7 @@ class AuthPage extends StatefulWidget {
   State<AuthPage> createState() => _AuthPageState();
 }
 
-class _AuthPageState extends State<AuthPage> {
+class _AuthPageState extends State<AuthPage> with TextThemes {
   final authController = GetIt.I.get<AuthController>();
   //TODO: remove if possible and make not necessary for GradientTextFieldWidget
   final FocusNode pinInputNode = FocusNode();
@@ -34,11 +36,18 @@ class _AuthPageState extends State<AuthPage> {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: mdSpacingx2),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: mdSpacingx2,
+              top: 104,
+            ),
             child: Align(
               alignment: Alignment.centerLeft,
-              child: TranslationText(textKey: 'login.authTitle'),
+              child: textH4(
+                context,
+                textKey: 'login.authTitle',
+                color: StandardColors.white,
+              ),
             ),
           ),
           Padding(
@@ -53,15 +62,47 @@ class _AuthPageState extends State<AuthPage> {
                       authController.pinFieldState != PinFieldState.invalid,
                   textController: pinTextController,
                   inputNode: pinInputNode,
-                  errorText: TranslationService.translate(
-                    context,
-                    'login.pinTextInputError',
-                  ),
+                  errorText:
+                      authController.pinFieldState != PinFieldState.invalid
+                          ? null
+                          : TranslationService.translate(
+                              context,
+                              'login.pinTextInputError',
+                            ),
                   hintText: '',
                 );
               },
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(top: xxlargeSpacing),
+            child: InAppKeyboardWidget(
+              onTap: (digit) {
+                final currentText = pinTextController.text;
+                String nextCurrentText = '';
+                if (digit == 'X') {
+                  final end =
+                      currentText.length - 1 < 0 ? 0 : currentText.length - 1;
+                  nextCurrentText = currentText.substring(0, end);
+                } else if (digit == 'del') {
+                  nextCurrentText = '';
+                } else {
+                  nextCurrentText = currentText + digit;
+                }
+                setState(() {
+                  pinTextController.text = nextCurrentText;
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: xxlargeSpacing * 2),
+            child: AnimatedArrowRight(
+              onTap: () {
+              },
+              isActive: false,
+            ),
+          )
         ],
       ),
     );
