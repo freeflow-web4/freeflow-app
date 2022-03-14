c59c43a535bfd0912f21cfe672cce73742ad69a9
Raphael <raphaeldesouza@outlook.com>
2022-03-14 17:37:40 -0300

    feat; add error color

--- lib/core/utils/colors_constants.dart
+++ lib/core/utils/colors_constants.dart
@@ -10,6 +10,7 @@ class StandardColors {
   static const Color grey = Color(0xffEFEFEF);
   static const Color white = Color(0xFFFCFCFC);
   static const Color darkGrey = Color(0xff4f4f4f);
+  static const Color error = Color(0xFFFF4747);
 
   static Gradient greenGradient() => const LinearGradient(
         colors: [
