8610cb6e2db4deee2b6bc17148811846428671d2
Raphael <raphaeldesouza@outlook.com>
2022-03-17 18:15:50 -0300

    feat: refactoring gradient text field

--- lib/layers/presentation/widgets/gradient_text_field_widget.dart
+++ lib/layers/presentation/widgets/gradient_text_field_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:freeflow/core/utils/assets_constants.dart';
 import 'package:freeflow/core/utils/colors_constants.dart';
+import 'package:freeflow/core/utils/spacing_constants.dart';
 import 'package:freeflow/core/utils/text_themes_mixin.dart';
 import 'package:freeflow/layers/presentation/widgets/gradient_text_field_pin_code.dart';
 
@@ -56,164 +57,132 @@ class _GradientTextFieldWidgetState extends State<GradientTextFieldWidget>
   Widget build(BuildContext context) {
     return Builder(
       builder: (context) {
-        return SizedBox(
-          height: 80,
-          child: Stack(
-            children: <Widget>[
-              widget.isObscureText == true
-                  ? SizedBox(
-                      width: double.infinity,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 13.0),
-                        child: GradientTextFieldPinCode(
-                          pinCode: widget.pinCode ?? '',
-                          color: widget.errorText?.isNotEmpty == true
-                              ? StandardColors.error
-                              : null,
-                        ),
-                      ),
-                    )
-                  : TextFormField(
-                      key: ValueKey('key_for_text_field${widget.pinCode}'),
-                      initialValue: widget.pinCode,
-                      onChanged: widget.onChanged,
-                      controller: widget.textController,
-                      readOnly: widget.fieldReadOnly,
-                      focusNode: widget.inputNode,
-                      style: TextStyle(
-                        color: widget.errorText == null
-                            ? widget.isFieldValid
-                                ? StandardColors.blueLight
-                                : Colors.white
-                            : StandardColors.feedbackError,
-                      ),
-                      decoration: InputDecoration(
-                        hintText: widget.isPinInput ? null : widget.hintText,
-                        hintStyle: TextStyle(
-                          color: widget.errorText == null
-                              ? Colors.white
-                              : StandardColors.feedbackError,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: 'Akrobat',
-                        ),
-                        border: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        focusedErrorBorder: InputBorder.none,
-                      ),
-                      maxLines: widget.maxLines,
-                    ),
-              Positioned(
-                top: widget.crossTheMaxLines ? 60 : 40,
-                child: Container(
-                  height: 2,
-                  width: MediaQuery.of(context).size.width - 20,
-                  decoration: BoxDecoration(
-                    gradient: widget.errorText == null
-                        ? StandardColors.greenGradient()
-                        : StandardColors.redGradient(),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: widget.crossTheMaxLines ? 60 : 40,
-                child: Container(
-                  height: 2,
-                  width: MediaQuery.of(context).size.width - 20,
-                  decoration: BoxDecoration(
-                    gradient: widget.errorText == null
-                        ? StandardColors.greenGradient()
-                        : StandardColors.redGradient(),
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: widget.errorText != null,
-                child: Positioned(
-                  bottom: widget.maxLines == 2 ? -2 : 10,
-                  child: textCaption(
-                    context,
-                    text: widget.errorText ?? '',
-                    color: widget.errorText == null
-                        ? Colors.white
-                        : StandardColors.feedbackError,
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: widget.isPinInput,
-                child: widget.isObscureText ?? false
-                    ? widget.pinCode != null && widget.pinCode!.isNotEmpty
-                        ? Positioned(
-                            top: 20,
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: <Widget>[
+                widget.isObscureText == true
+                    ? SizedBox(
+                        width: double.infinity,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            top: 18,
+                            bottom: 18,
+                          ),
+                          child: Container(
+                            height: 12,
+                            alignment: Alignment.centerLeft,
                             child: GradientTextFieldPinCode(
-                              pinCode: widget.pinCode!,
+                              pinCode: widget.pinCode ?? '',
+                              color: widget.errorText?.isNotEmpty == true
+                                  ? StandardColors.error
+                                  : null,
                             ),
-                          )
-                        : Positioned(
-                            top: 13,
-                            child: textSubtitle(
-                              context,
-                              textKey: widget.hintText,
-                              color: widget.errorText == null
-                                  ? Colors.white
-                                  : StandardColors.feedbackError,
-                            ),
-                          )
-                    : Positioned(
-                        top: 13,
-                        child: textSubtitle(
-                          context,
-                          textKey: widget.pinCode ?? '',
+                          ),
+                        ),
+                      )
+                    : TextFormField(
+                        key: ValueKey(
+                          'key_for_text_field${widget.pinCode}',
+                        ),
+                        initialValue: widget.pinCode,
+                        onChanged: widget.onChanged,
+                        controller: widget.textController,
+                        readOnly: widget.fieldReadOnly,
+                        focusNode: widget.inputNode,
+                        style: TextStyle(
                           color: widget.errorText == null
-                              ? Colors.white
+                              ? widget.isFieldValid
+                                  ? StandardColors.blueLight
+                                  : Colors.white
                               : StandardColors.feedbackError,
                         ),
+                        decoration: InputDecoration(
+                          hintText: widget.isPinInput ? null : widget.hintText,
+                          hintStyle: TextStyle(
+                            color: widget.errorText == null
+                                ? Colors.white
+                                : StandardColors.feedbackError,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: 'Akrobat',
+                          ),
+                          contentPadding: const EdgeInsets.all(0),
+                          border: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          focusedErrorBorder: InputBorder.none,
+                        ),
+                        maxLines: widget.maxLines,
                       ),
-              ),
-              Visibility(
-                visible: widget.showSecondText,
-                child: Positioned(
-                  top: 13,
-                  right: 0,
-                  child: textBoldSubtitle(
-                    context,
-                    textKey: '.flw ',
-                    color: widget.errorText == null
-                        ? widget.isFieldValid
-                            ? StandardColors.blueLight
-                            : Colors.white
-                        : StandardColors.feedbackError,
-                  ),
-                ),
-              ),
-              Visibility(
-                visible: widget.showObscureButton,
-                child: Positioned(
-                  right: 0,
-                  top: 12,
-                  child: GestureDetector(
-                    behavior: HitTestBehavior.opaque,
-                    onTap: widget.onObscureButtonPressed,
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      alignment: Alignment.center,
-                      child: SvgPicture.asset(
-                        widget.isObscureText == true
-                            ? IconsAsset.closedEye
-                            : IconsAsset.openEye,
+                Visibility(
+                  visible: widget.showObscureButton,
+                  child: Positioned(
+                    right: 0,
+                    top: 14,
+                    child: GestureDetector(
+                      behavior: HitTestBehavior.opaque,
+                      onTap: widget.onObscureButtonPressed,
+                      child: Container(
                         width: 20,
-                        color: widget.obscureButtonColor,
+                        height: 20,
+                        alignment: Alignment.center,
+                        child: SvgPicture.asset(
+                          widget.isObscureText == true
+                              ? IconsAsset.closedEye
+                              : IconsAsset.openEye,
+                          width: 20,
+                          color: widget.obscureButtonColor,
+                        ),
                       ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            Visibility(
+              visible: widget.showSecondText,
+              child: Positioned(
+                top: 13,
+                right: 0,
+                child: textBoldSubtitle(
+                  context,
+                  textKey: '.flw ',
+                  color: widget.errorText == null
+                      ? widget.isFieldValid
+                          ? StandardColors.blueLight
+                          : Colors.white
+                      : StandardColors.feedbackError,
+                ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              height: 2,
+              padding: EdgeInsets.only(top: widget.crossTheMaxLines ? 20 : 0),
+              width: MediaQuery.of(context).size.width - 20,
+              decoration: BoxDecoration(
+                gradient: widget.errorText == null
+                    ? StandardColors.greenGradient()
+                    : StandardColors.redGradient(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: miniSpacing,
+                bottom: widget.maxLines == 2 ? -2 : 10,
+              ),
+              child: textCaption(
+                context,
+                text: widget.errorText ?? '',
+                color: widget.errorText == null
+                    ? Colors.white
+                    : StandardColors.feedbackError,
+              ),
+            )
+          ],
         );
       },
     );
