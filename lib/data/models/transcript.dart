40ee6cad3a528df94051fcdb7a31a52fcce98caf
Gracyele <gracyelesantossj@gmail.com>
2022-04-04 20:32:43 -0300

    feat: create model and mock test

---
+++ lib/data/models/transcript.dart
@@ -0,0 +1,15 @@
+class Transcript {
+  final String category;
+  final String amount;
+  final String gratitudeType;
+  final String createdAt;
+  final String userName;
+
+  Transcript({
+    required this.category,
+    required this.amount,
+    required this.gratitudeType,
+    required this.createdAt,
+    required this.userName,
+  });
+}
---
+++ lib/layers/presentation/pages/wallet/mock.dart
@@ -0,0 +1,41 @@
+import 'package:freeflow/data/models/transcript.dart';
+
+abstract class Mock {
+  static final List<Transcript> transcripts = [
+    Transcript(
+      amount: '102',
+      category: 'gratitude',
+      createdAt: '6 ds 21h',
+      gratitudeType: 'NFT',
+      userName: 'fulano',
+    ),
+    Transcript(
+      amount: '102',
+      category: 'gratitude',
+      createdAt: '6 ds 21h',
+      gratitudeType: 'NFT',
+      userName: 'fulano',
+    ),
+    Transcript(
+      amount: '102',
+      category: 'flower_exchange',
+      createdAt: '6 ds 21h',
+      gratitudeType: 'NFT',
+      userName: 'fulano',
+    ),
+    Transcript(
+      amount: '102',
+      category: 'inter_action',
+      createdAt: '6 ds 21h',
+      gratitudeType: 'NFT',
+      userName: 'fulano',
+    ),
+    Transcript(
+      amount: '102',
+      category: 'inter_action',
+      createdAt: '6 ds 21h',
+      gratitudeType: 'NFT',
+      userName: 'fulano',
+    ),
+  ];
+}
