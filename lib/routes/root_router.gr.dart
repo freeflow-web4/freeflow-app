9ccb6a687739b3be3297fc2bd257917debb693ea
Raphael <raphaeldesouza@outlook.com>
2022-04-05 18:25:28 -0300

    feat: update gr code

--- lib/routes/root_router.gr.dart
+++ lib/routes/root_router.gr.dart
@@ -10,8 +10,8 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i9;
-import 'package:flutter/material.dart' as _i10;
+import 'package:auto_route/auto_route.dart' as _i10;
+import 'package:flutter/material.dart' as _i11;
 
 import '../layers/presentation/pages/auth/auth_page.dart' as _i6;
 import '../layers/presentation/pages/create_wallet/create_wallet_page.dart'
@@ -23,84 +23,81 @@ import '../layers/presentation/pages/login/login_page.dart' as _i2;
 import '../layers/presentation/pages/recover_account/recover_account_page.dart'
     as _i5;
 import '../layers/presentation/pages/recover_splash/recover_splash_page.dart'
-    as _i8;
+    as _i9;
 import '../layers/presentation/pages/splash/splash_page.dart' as _i3;
 import '../layers/presentation/pages/welcome/welcome_page.dart' as _i1;
 
-class RootRouter extends _i9.RootStackRouter {
-  RootRouter([_i10.GlobalKey<_i10.NavigatorState>? navigatorKey])
+class RootRouter extends _i10.RootStackRouter {
+  RootRouter([_i11.GlobalKey<_i11.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i9.PageFactory> pagesMap = {
+  final Map<String, _i10.PageFactory> pagesMap = {
     WelcomeRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i10.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.WelcomePage());
     },
-    SplashRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i2.SplashPage());
-    },
     LoginRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i3.LoginPage());
+      return _i10.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i2.LoginPage());
+    },
+    SplashRoute.name: (routeData) {
+      return _i10.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i3.SplashPage());
     },
     FreeflowLogoLoadingRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i10.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i4.FreeflowLogoLoadingPage());
     },
     RecoverAccountRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i10.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i5.RecoverAccountPage());
     },
     AuthRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i10.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i6.AuthPage());
     },
     HomeRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i10.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i7.HomePage());
     },
     CreateWalletRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i10.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i8.CreateWalletPage());
+    },
+    RecoverSplashRoute.name: (routeData) {
+      return _i10.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i9.RecoverSplashPage());
     }
   };
 
   @override
-  List<_i9.RouteConfig> get routes => [
-        _i9.RouteConfig(WelcomeRoute.name, path: '/welcome-page'),
-        _i9.RouteConfig(SplashRoute.name, path: '/splash-page'),
-        _i9.RouteConfig(LoginRoute.name, path: '/login-page'),
-        _i9.RouteConfig(FreeflowLogoLoadingRoute.name,
+  List<_i10.RouteConfig> get routes => [
+        _i10.RouteConfig(WelcomeRoute.name, path: '/welcome-page'),
+        _i10.RouteConfig(LoginRoute.name, path: '/login-page'),
+        _i10.RouteConfig(SplashRoute.name, path: '/'),
+        _i10.RouteConfig(FreeflowLogoLoadingRoute.name,
             path: '/freeflow-logo-loading-page'),
-        _i9.RouteConfig(RecoverAccountRoute.name,
+        _i10.RouteConfig(RecoverAccountRoute.name,
             path: '/recover-account-page'),
-        _i9.RouteConfig(AuthRoute.name, path: '/auth-page'),
-        _i9.RouteConfig(HomeRoute.name, path: '/home-page'),
-        _i9.RouteConfig(CreateWalletRoute.name, path: '/')
+        _i10.RouteConfig(AuthRoute.name, path: '/auth-page'),
+        _i10.RouteConfig(HomeRoute.name, path: '/home-page'),
+        _i10.RouteConfig(CreateWalletRoute.name, path: '/create-wallet-page'),
+        _i10.RouteConfig(RecoverSplashRoute.name, path: '/recover-splash-page')
       ];
 }
 
 /// generated route for
 /// [_i1.WelcomePage]
-class WelcomeRoute extends _i9.PageRouteInfo<void> {
+class WelcomeRoute extends _i10.PageRouteInfo<void> {
   const WelcomeRoute() : super(WelcomeRoute.name, path: '/welcome-page');
 
   static const String name = 'WelcomeRoute';
 }
 
 /// generated route for
-/// [_i2.SplashPage]
-class SplashRoute extends _i9.PageRouteInfo<void> {
-  const SplashRoute() : super(SplashRoute.name, path: '/splash-page');
-
-  static const String name = 'SplashRoute';
-}
-
-/// generated route for
-/// [_i3.LoginPage]
-class LoginRoute extends _i9.PageRouteInfo<void> {
+/// [_i2.LoginPage]
+class LoginRoute extends _i10.PageRouteInfo<void> {
   const LoginRoute() : super(LoginRoute.name, path: '/login-page');
 
   static const String name = 'LoginRoute';
@@ -108,7 +105,7 @@ class LoginRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.SplashPage]
-class SplashRoute extends _i9.PageRouteInfo<void> {
+class SplashRoute extends _i10.PageRouteInfo<void> {
   const SplashRoute() : super(SplashRoute.name, path: '/');
 
   static const String name = 'SplashRoute';
@@ -116,7 +113,7 @@ class SplashRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i4.FreeflowLogoLoadingPage]
-class FreeflowLogoLoadingRoute extends _i9.PageRouteInfo<void> {
+class FreeflowLogoLoadingRoute extends _i10.PageRouteInfo<void> {
   const FreeflowLogoLoadingRoute()
       : super(FreeflowLogoLoadingRoute.name,
             path: '/freeflow-logo-loading-page');
@@ -126,7 +123,7 @@ class FreeflowLogoLoadingRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i5.RecoverAccountPage]
-class RecoverAccountRoute extends _i9.PageRouteInfo<void> {
+class RecoverAccountRoute extends _i10.PageRouteInfo<void> {
   const RecoverAccountRoute()
       : super(RecoverAccountRoute.name, path: '/recover-account-page');
 
@@ -135,7 +132,7 @@ class RecoverAccountRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i6.AuthPage]
-class AuthRoute extends _i9.PageRouteInfo<void> {
+class AuthRoute extends _i10.PageRouteInfo<void> {
   const AuthRoute() : super(AuthRoute.name, path: '/auth-page');
 
   static const String name = 'AuthRoute';
@@ -143,7 +140,7 @@ class AuthRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i7.HomePage]
-class HomeRoute extends _i9.PageRouteInfo<void> {
+class HomeRoute extends _i10.PageRouteInfo<void> {
   const HomeRoute() : super(HomeRoute.name, path: '/home-page');
 
   static const String name = 'HomeRoute';
@@ -151,8 +148,18 @@ class HomeRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i8.CreateWalletPage]
-class CreateWalletRoute extends _i9.PageRouteInfo<void> {
-  const CreateWalletRoute() : super(CreateWalletRoute.name, path: '/');
+class CreateWalletRoute extends _i10.PageRouteInfo<void> {
+  const CreateWalletRoute()
+      : super(CreateWalletRoute.name, path: '/create-wallet-page');
 
   static const String name = 'CreateWalletRoute';
+}
+
+/// generated route for
+/// [_i9.RecoverSplashPage]
+class RecoverSplashRoute extends _i10.PageRouteInfo<void> {
+  const RecoverSplashRoute()
+      : super(RecoverSplashRoute.name, path: '/recover-splash-page');
+
+  static const String name = 'RecoverSplashRoute';
 }
