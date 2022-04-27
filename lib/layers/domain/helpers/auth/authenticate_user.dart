f1efee701d30ea3151bcb0277a03a4638f00fd07
Raphael <raphaeldesouza@outlook.com>
2022-04-27 15:54:30 -0300

    feat: add auth helper

---
+++ lib/layers/domain/helpers/auth/authenticate_user.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:freeflow/core/utils/text_themes_mixin.dart';
+import 'package:freeflow/layers/infra/route/route_response.dart';
+import 'package:freeflow/layers/presentation/helpers/show_flex_bottom_sheet.dart';
+import 'package:freeflow/layers/presentation/pages/logout/pages/auth/logout_auth_page.dart';
+
+Future<bool> authenticateUser(BuildContext context) async {
+  final authResult = await showFlexBottomSheet<RouteResponse?>(
+    context,
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _Text().text(context),
+      ],
+    ),
+    const LogoutAuthPage(),
+  );
+  final auth = authResult?.body ?? false;
+  return auth;
+}
+
+class _Text with TextThemes {
+  Widget text(BuildContext context) {
+    return textH6(
+      context,
+      textKey: 'logout.authTitle',
+      isUpperCase: true,
+    );
+  }
+}
