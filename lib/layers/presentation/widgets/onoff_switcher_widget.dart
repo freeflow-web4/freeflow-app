d91231b8e8a39d5c8bd786c55eafdfca79ff1dc9
Raphael <raphaeldesouza@outlook.com>
2022-04-27 10:27:05 -0300

    feat: add onoff switcher

---
+++ lib/layers/presentation/widgets/onoff_switcher_widget.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:freeflow/core/utils/colors_constants.dart';
+import 'package:freeflow/core/utils/text_themes_mixin.dart';
+
+class OnOffSwitcher extends StatefulWidget {
+  final bool value;
+  final void Function(bool value) onChanged;
+  const OnOffSwitcher({
+    required this.value,
+    required this.onChanged,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<OnOffSwitcher> createState() => _OnOffSwitcherState();
+}
+
+class _OnOffSwitcherState extends State<OnOffSwitcher> with TextThemes {
+  static const _borderRadius = 20.0;
+  static const _circleSize = 30.0;
+  static const _animationDuration = 200;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => widget.onChanged(!widget.value),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: _animationDuration),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(
+            _borderRadius,
+          ),
+          color: widget.value ? StandardColors.success : StandardColors.error,
+        ),
+        padding: const EdgeInsets.all(5),
+        child: SizedBox(
+          width: 110,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 11.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Visibility(
+                          visible: widget.value,
+                          child: Text(
+                            "ON",
+                            style: textSubtitle3Style(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                        Visibility(
+                          visible: !widget.value,
+                          child: Text(
+                            "OFF",
+                            style: textSubtitle3Style(
+                              color: Colors.white,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              AnimatedAlign(
+                duration: const Duration(milliseconds: _animationDuration),
+                alignment:
+                    widget.value ? Alignment.centerRight : Alignment.centerLeft,
+                curve: Curves.ease,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  width: _circleSize,
+                  height: _circleSize,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
