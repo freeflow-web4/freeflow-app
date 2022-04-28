1289068ce595f8dca8cf4fee080d3cb198e4d540
Raphael <raphaeldesouza@outlook.com>
2022-04-28 18:57:03 -0300

    fix: add changes for freef-56

--- lib/layers/presentation/helpers/show_fullscreen_dialog.dart
+++ lib/layers/presentation/helpers/show_fullscreen_dialog.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:freeflow/core/utils/assets_constants.dart';
 import 'package:freeflow/layers/presentation/pages/fullscreen_alert_dialog/fullscreen_alert_dialog.dart';
 
 Future showCustomDialog(
   BuildContext context, {
-  required String textKey,
+  String? textKey,
+  String? text,
   String? icon = '',
-  String? secondaryTextKey = '',
+  String? secondaryTextKey,
+  String? secondaryText,
+  Widget? body,
+  SecondaryTextPosition secondaryTextPosition = SecondaryTextPosition.bottom,
 }) {
   return showGeneralDialog(
     context: context,
     pageBuilder: (BuildContext context, animation1, animation2) {
       return FullScreenAlertDialog(
-        textKey: FlutterI18n.translate(
-          context,
-          textKey,
-        ),
+        body: body,
+        textKey: textKey,
+        text: text,
         icon: icon == '' ? null : icon,
         secondaryTextKey: secondaryTextKey == '' ? null : secondaryTextKey,
+        secondaryText: secondaryText,
+        secondaryTextPosition: secondaryTextPosition,
       );
     },
   );
