b3fb1279d020ea551329d47324e5e305da86be07
arielsam567 <Saidemim112104>
2022-05-02 19:32:50 -0300

    fix: does not allow accessing the collectibles and FLWR

--- lib/layers/presentation/widgets/custom_tabbar.dart
+++ lib/layers/presentation/widgets/custom_tabbar.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:freeflow/core/utils/colors_constants.dart';
 import 'package:freeflow/core/utils/text_themes_mixin.dart';
+import 'package:freeflow/layers/presentation/helpers/dialog/show_dialog_default.dart';
 
-class CustomTabBar extends StatelessWidget with TextThemes {
+class CustomTabBar extends StatefulWidget {
   final List<Widget> tabBarViewList;
   final double? width, height;
   final List<Tab> tabList;
@@ -14,8 +15,45 @@ class CustomTabBar extends StatelessWidget with TextThemes {
     required this.tabList,
     this.height,
     this.width,
-    this.indicatorDecoration, 
+    this.indicatorDecoration,
   }) : super(key: key);
+
+  @override
+  State<CustomTabBar> createState() => _CustomTabBarState();
+}
+
+class _CustomTabBarState extends State<CustomTabBar>  with TextThemes , SingleTickerProviderStateMixin{
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3);
+    ///TODO
+    ///REMOVE WHEN FEATURE IS AVAILABLE
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        showDialogDefault(
+          context,
+          automaticallyCloses: true,
+          type: DialogType.featureNotAvailable,
+          onTap: () {},
+        );
+        Future.delayed(const Duration(milliseconds: 500), (){
+          setState(() {
+            _tabController.index = 0;
+          });
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,23 +63,24 @@ class CustomTabBar extends StatelessWidget with TextThemes {
         splashColor: Colors.transparent,
       ),
       child: DefaultTabController(
-        length: tabList.length,
+        length: widget.tabList.length,
         child: Column(
           children: [
             TabBar(
+              controller: _tabController,
               indicatorSize: TabBarIndicatorSize.label,
               unselectedLabelColor:
-                  StandardColors.textMediumGrey.withOpacity(0.6),
+              StandardColors.textMediumGrey.withOpacity(0.6),
               labelColor: StandardColors.darkGrey,
               padding: EdgeInsets.zero,
               indicatorPadding: EdgeInsets.zero,
               labelPadding: EdgeInsets.zero,
-              tabs: tabList,
-              indicator: indicatorDecoration,
+              tabs: widget.tabList,
+              indicator: widget.indicatorDecoration,
             ),
             Expanded(
               child: TabBarView(
-                children: tabBarViewList,
+                children: widget.tabBarViewList,
               ),
             ),
           ],
